c36579784ec4180c74486f47d09a73c79633f607
nazrin ali <nali3171@outlook.com>
2025-06-17 15:20:25 +0100

    Completed Challenge 4: Print list items with index

--- day1_basics/challenges.py
+++ day1_basics/challenges.py
@@ -6,11 +6,23 @@ for num in numbers:
         print(num)
 
 # 2. Create a dictionary mapping numbers to their squares (1–5).
+squares = {x: x**2 for x in range(1, 6)}
+print(squares)
 
 
 
 # 3. Add an item to a dictionary and remove one.
+person ={
+    "name": "Naz",
+    "age": 23
+}
+person ["job"] = "doctor"
+person.pop("age")
 
+print(person)
 
 
 # 4. Loop through a list of fruits and print each one.
+fruits = ["apple", "banana", "cherry"]
+for index, fruit in enumerate(fruits):
+    print(f"{index}:{fruit}")
